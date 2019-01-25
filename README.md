60384461470c846fd1ea3ecaa87c988d7a3b9c39
iolumbaram <hazel77744@gmail.com>
2019-01-25 13:22:56 +0800

    read vcf

---
+++ README.md
@@ -0,0 +1,2 @@
+https://en.wikipedia.org/wiki/Unified_Hangul_Code
+https://libsora.so/posts/python-hangul/
--- utf8decoder.py
+++ utf8decoder.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
+import codecs
+
+f = codecs.open("../list.vcf", "r", "utf-8")
+# f.read()
+for line in f:
+    print(type(line))
+    # line.find(';=')
 
 s = '\xED\x95\x9C\xEA\xB8\x80'
 #s = '한글'
-print s.decode('utf-8').encode('utf-8')
+#print s.decode('utf-8').encode('utf-8')
+
